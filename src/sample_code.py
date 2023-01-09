2ebf7d023ff259bc4efad0988f95368d98fe203c
smakulp <przemyslaw.smakulski@technipfmc.com>
2023-01-09 14:44:22 +0100

    (sample_code): refactor

--- src/sample_code.py
+++ src/sample_code.py
@@ -14,27 +14,27 @@ def formulation_Custom(f_range, bb):
     df = pd.read_excel('force-psds-custom.xlsx', sheet_name='Custom')
     freqs = pd.to_numeric(df['Frequency [Hz]'])
     if bb < 10:
-        bendcolumn = 'b0' + str(bb+1)
+        bendcolumn = 'b0' + str(bb + 1)
     else:
-        bendcolumn = 'b' + str(bb+1)
-        
+        bendcolumn = 'b' + str(bb + 1)
+
     psd = pd.to_numeric(df[bendcolumn])
-    
-    Frms = np.sqrt(np.trapz(psd,x=freqs))
-    
+
+    Frms = np.sqrt(np.trapz(psd, x=freqs))
+
     df = f_range[-1] - f_range[-2]
-    
+
     freqlimit = (f_range[-1] + 20.)
     if freqs.iloc[-1] < freqlimit:
         a = np.arange(freqs.iloc[-1], (f_range[-1] + 20.), df)
         b = np.zeros_like(a)
-        fi = np.hstack((freqs,a))
-        psdi = np.hstack((psd,b))
+        fi = np.hstack((freqs, a))
+        psdi = np.hstack((psd, b))
     else:
         fi = freqs
         psdi = psd
 
     func = interpolate.interp1d(fi, psdi)
     phi = func(f_range)
-    
+
     return (Frms, phi)
